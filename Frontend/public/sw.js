a1ff5b584e928ba655c1127d12e3ef5eb00920b4
mayurpatil-in <ai.mayurpatil@gmail.com>
2026-03-01 02:38:05 +0530

    PWA app added

---
+++ Frontend/public/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "pomodoro-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/goalt.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
